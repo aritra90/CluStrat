78faa5a8460bba4c544806d5ab3fc49c4b392cff
mchburch <45465336+mchburch@users.noreply.github.com>
2019-01-20 07:42:33 -0500

    Updated clean.sh to include additional files

--- clean.sh
+++ clean.sh
@@ -7,10 +7,12 @@ rm -fr PRK_Ridge_*
 rm -fr PRK_Lasso_*
 rm -fr PRK_Elastic_*
 rm -fr PRK_SVM_*
+rm -fr PRK_SVR_*
 rm -fr PRK_kSVM_*
 rm -fr PRK_RidgeSVM_*
 rm -fr PRK_RFESVM_*
 rm -fr PRK_RandomForest_*
+rm -fr PRK_RidgeCV_*
 rm -f PKHCGSRRS_extracted_*
 
 rm -fr SCZ_LDA_*
@@ -20,10 +22,12 @@ rm -fr SCZ_Ridge_*
 rm -fr SCZ_Lasso_*
 rm -fr SCZ_Elastic_*
 rm -fr SCZ_SVM_*
+rm -fr SCZ_SVR_*
 rm -fr SCZ_kSVM_*
 rm -fr SCZ_RidgeSVM_*
 rm -fr SCZ_RFESVM_*
 rm -fr SCZ_RandomForest_*
+rm -fr SCZ_RidgeCV_*
 rm -f SZHCGSRRS_extracted_*
 
 rm -f *.out 
@@ -32,4 +36,7 @@ rm -f *bestaccuracy.txt
 rm -f *f1score.txt
 rm -f *positiveaccuracy.txt
 rm -f *negativeaccuracy.txt
+
+rm -f mysonMLGWASJob.*
+rm -f *.png
 #END
