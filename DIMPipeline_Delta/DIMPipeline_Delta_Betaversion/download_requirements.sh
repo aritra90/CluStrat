e7efe6147a373e9bc9eb49dd36cc91dca78741bd
Myson Burch <mysonburch@gmail.com>
2019-02-21 09:29:44 -0500

    Updated Delta

--- DIMPipeline_Delta/DIMPipeline_Delta_Betaversion/download_requirements.sh
+++
@@ -1,6 +0,0 @@
-module use /depot/itap/amaji/modules
-module spider learning
-module load learning/conda-5.1.0-py27-cpu
-module --terse spider ml-toolkit-cpu
-module load ml-toolkit-cpu/all
-module list
